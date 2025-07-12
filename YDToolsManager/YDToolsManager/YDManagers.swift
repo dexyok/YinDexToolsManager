9317742d1fe30ac7a1fb44530dd9e46fec55908c
dexyok <dexterousYokel@outlook.com>
2025-07-13 01:27:28 +0200

    dexyok
    创建项目

---
+++ YDToolsManager/YDToolsManager/YDManagers.swift
@@ -0,0 +1,16 @@
+//
+//  YDManagers.swift
+//  YDToolsManager
+//
+//  Created by paris on 12/07/25.
+//
+
+import UIKit
+
+class YDManagers: NSObject {
+
+    /// appid
+    static let lullaby = "liupeice2"
+    /// salt
+    static let solitude = "QvfBEKqVDNJjwkSG"
+}
---
+++ YDToolsManager/YDToolsManager/YDToolsManager.swift
@@ -0,0 +1,9 @@
+//
+//  YDToolsManager.swift
+//  YDToolsManager
+//
+//  Created by paris on 12/07/25.
+//
+
+import Foundation
+
